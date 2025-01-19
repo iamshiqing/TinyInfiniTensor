286dd1cabf4fc8600837914eb98e43bcf3d86654
iamshiqing <183805476@qq.com>
2025-01-19 12:29:04 +0800

    new

--- src/operators/unary.cc
+++ src/operators/unary.cc
@@ -39,7 +39,7 @@ namespace infini
         // TODO：返回经过 clip 操作后的 shape
         // REF: https://onnx.ai/onnx/operators/onnx__Clip.html#clip-13
         // =================================== 作业 ===================================
-        return std::nullopt;
+        return {{inputs[0]->getDims()}};
     }
 
     std::string ClipObj::toString() const
